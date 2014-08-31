c0cd22603acf08a5630e07d24748d92cef6c83af
Lee Mason <contact@nohalfpixels.com>
2014-08-31 17:33:52 +0000

    ...

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylorotwell@gmail.com>
+ */
+
+require __DIR__ . '/start.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can simply call the run method,
+| which will execute the request and send the response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have whipped up for them.
+|
+*/
+
+$app->run();
---
+++ start.php
@@ -0,0 +1,193 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for our application. We just need to utilize it! We'll require it
+| into the script here so that we do not have to worry about the
+| loading of any our classes "manually". Feels great to relax.
+|
+*/
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Register The Composer Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for our application. We just need to utilize it! We'll require it
+| into the script here so that we do not have to worry about the
+| loading of any our classes "manually". Feels great to relax.
+|
+*/
+
+require __DIR__.'/vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Include The Compiled Class File
+|--------------------------------------------------------------------------
+|
+| To dramatically increase your application's performance, you may use a
+| compiled class file which contains all of the classes commonly used
+| by a request. The Artisan "optimize" is used to create this file.
+|
+*/
+
+$compiledPath = __DIR__.'/storage/meta/compiled.php';
+
+if (file_exists($compiledPath))
+{
+	require $compiledPath;
+}
+
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let's turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight these users.
+|
+*/
+
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+|
+| The first thing we will do is create a new Laravel application instance
+| which serves as the "glue" for all the components of Laravel, and is
+| the IoC container for the system binding all of the various parts.
+|
+*/
+
+$app = new Illuminate\Foundation\Application;
+
+/*
+|--------------------------------------------------------------------------
+| Detect The Application Environment
+|--------------------------------------------------------------------------
+|
+| Laravel takes a dead simple approach to your application environments
+| so you can just specify a machine name for the host that matches a
+| given environment, then we will automatically detect it for you.
+|
+*/
+
+$env = $app->detectEnvironment([
+
+	'local' => ['homestead'],
+
+]);
+
+/*
+|--------------------------------------------------------------------------
+| Bind Paths
+|--------------------------------------------------------------------------
+|
+| Here we are binding the paths configured in paths.php to the app. You
+| should not be changing these here. If you need to change these you
+| may do so within the paths.php file and they will be bound here.
+|
+*/
+
+$app->bindInstallPaths([
+
+	/*
+	|--------------------------------------------------------------------------
+	| Application Path
+	|--------------------------------------------------------------------------
+	|
+	| Here we just defined the path to the application directory. Most likely
+	| you will never need to change this value as the default setup should
+	| work perfectly fine for the vast majority of all our applications.
+	|
+	*/
+
+	'app' => __DIR__.'/app',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Public Path
+	|--------------------------------------------------------------------------
+	|
+	| The public path contains the assets for your web application, such as
+	| your JavaScript and CSS files, and also contains the primary entry
+	| point for web requests into these applications from the outside.
+	|
+	*/
+
+	'public' => __DIR__,
+
+	/*
+	|--------------------------------------------------------------------------
+	| Base Path
+	|--------------------------------------------------------------------------
+	|
+	| The base path is the root of the Laravel installation. Most likely you
+	| will not need to change this value. But, if for some wild reason it
+	| is necessary you will do so here, just proceed with some caution.
+	|
+	*/
+
+	'base' => __DIR__,
+
+	/*
+	|--------------------------------------------------------------------------
+	| Storage Path
+	|--------------------------------------------------------------------------
+	|
+	| The storage path is used by Laravel to store cached Blade views, logs
+	| and other pieces of information. You may modify the path here when
+	| you want to change the location of this directory for your apps.
+	|
+	*/
+
+	'storage' => __DIR__.'/storage',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Generator Paths
+	|--------------------------------------------------------------------------
+	|
+	| These paths are used by the various class generators and other pieces
+	| of the framework that need to determine where to store these types
+	| of classes. Of course, they may be changed to any path you wish.
+	|
+	*/
+
+	'console' => __DIR__.'/app/Console',
+	'config' => __DIR__.'/app/config',
+	'controllers' => __DIR__.'/app/Http/Controllers',
+	'database' => __DIR__.'/app/database',
+	'filters' => __DIR__.'/app/Http/Filters',
+	'lang' => __DIR__.'/resources/lang',
+	'providers' => __DIR__.'/app/Providers',
+	'requests' => __DIR__.'/app/Http/Requests',
+
+]);
+
+/*
+|--------------------------------------------------------------------------
+| Load The Application
+|--------------------------------------------------------------------------
+|
+| Here we will load this Illuminate application. We will keep this in a
+| separate location so we can isolate the creation of an application
+| from the actual running of the application with a given request.
+|
+*/
+
+$framework = $app['path.base'].
+                 '/vendor/laravel/framework/src';
+
+require $framework.'/Illuminate/Foundation/start.php';
+
+return $app;
